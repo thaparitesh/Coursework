6dcc3e3648f516f4722499ce3a6f9883c384cad6
abinashchhetri <abinashchhetri.work@gmail.com>
2024-12-19 09:14:47 -0600

    Add project files.

---
+++ Coursework/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Coursework
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Coursework/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Coursework
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
